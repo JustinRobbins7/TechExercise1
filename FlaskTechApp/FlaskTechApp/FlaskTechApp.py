22139c6c9079da251581dcf9fb616e6ccbf79774
JustinRobbins7 <robbinsju@gmail.com>
2018-09-11 23:39:50 +0000

    Commit before trying to deploy to server

--- FlaskTechApp/FlaskTechApp/FlaskTechApp.py
+++ FlaskTechApp/FlaskTechApp/FlaskTechApp.py
@@ -1,28 +1,65 @@
 from flask import Flask, render_template, request, json
-import datetime, time
+from flaskext.mysql import MySQL
+from datetime import datetime
+from time import strptime
 
 app = Flask(__name__)
+mysql = MySQL()
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MYSQL_DATABASE_USER'] = 'justinrobbins'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Jayman77'
+app.config['MYSQL_DATABASE_DB'] = 'FlaskTechApp'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return ReloadData()
 
 @app.route("/",methods=['POST'])
-def enterEvent():
+def handleInput():
+    
     _event_name = request.form['inputEvent']
     _event_loc = request.form['inputLoc']
     _event_desc = request.form['inputDesc']
     _html_date_input = request.form['inputDateTime']
-    """ 
-    _html_date = strptime(_html_date_input, "%b. %d, %Y, %I %p")
-    _event_date = strftime(_html_date, "%Y-%m-%d %H:%M:%S")
-    """
-
+     
+    _html_date = datetime(*(strptime(_html_date_input, "%Y-%m-%dT%H:%M")[0:6]))
+    _event_date = datetime.strftime(_html_date, "%Y-%m-%d %H:%M:%S")
+    
     if _event_name and _event_loc and _event_desc and _html_date_input:
-        return json.dumps({'html':'<span>'+ _html_date_input +'</span>'})
+        return EnterIntoDB((_event_name, _event_loc, _event_date, _event_desc))
     else:
         return json.dumps({'html':'<span>Enter the required fields</span>'})
 
+def EnterIntoDB(argtuple):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.callproc('create_event',argtuple)
+    data = cursor.fetchall()
+    
+    if len(data) is 0:
+        conn.commit()
+        conn.close()
+        return json.dumps({'message':'entry successful!'})
+    else:
+        conn.close()
+        return json.dumps({'error':str(data[0])})
+
+def ReloadData():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM events')
+    events = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', events = events)  
 
 if __name__ == "__main__":
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('TRUNCATE TABLE events')
+    events = cursor.fetchall()
+    conn.commit()
+    conn.close()
     app.run(host='0.0.0.0', port='8080')
